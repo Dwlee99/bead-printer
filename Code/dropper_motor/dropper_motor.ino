8d1a06736170ebe5220f968cf1b2821ee2e266a9
Rachel Zhou <sz287@cornell.edu>
2023-03-17 16:02:11 -0400

    motor code

---
+++ Code/dropper_motor/dropper_motor.ino
@@ -0,0 +1,22 @@
+#include <ESP32_Servo.h>
+const int servo_pin = 13;
+
+int pos = 0;    // variable to store the servo position
+Servo myservo;
+
+void setup() {
+  myservo.attach(servo_pin,500,2400);  
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+}
