779597b3290ee862cf65ba03adb2e2f9fdf6b25e
Rachel Zhou <sz287@cornell.edu>
2023-04-19 14:11:28 -0400

    added bearings

--- Code/dropper_motor/dropper_motor.ino
+++ Code/dropper_motor/dropper_motor.ino
@@ -13,10 +13,10 @@ void loop() {
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+    delay(10);                       // waits 15ms for the servo to reach the position
   }
 }
